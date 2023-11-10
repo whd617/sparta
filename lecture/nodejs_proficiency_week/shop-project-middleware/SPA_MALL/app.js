dda41748ffe45ac371429d5ee8a4d5850171f665
Jonghyeon Choi <whd617@naver.com>
2023-11-10 23:48:50 +0900

    Shopping mall project nodejs proficiency study

--- lecture/nodejs_proficiency_week/shop-project-middleware/SPA_MALL/app.js
+++ lecture/nodejs_proficiency_week/shop-project-middleware/SPA_MALL/app.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 const goodsRouter = require("./routes/goods");
 const cartsRouter = require("./routes/carts.js");
 
 const connect = require("./schemas");
+const router = require("./routes/goods");
+const User = require("./models/user.js");
 connect();
+
+router.post("/users", async (req, res) => {
+    const { nickname, email, password, confirmpassword } = req.body;
+
+    // 1. 패스워드, 패스워드 검증 값이 일치하는가
+    // 2. email에 해당하는 사용자가 있는가
+    // 3. nicname에 해당하는 사용자가 있는가
+    // 4. DB에 데이터를 삽입
+
+    if (password !== confirmpassword) {
+        res.status(400).json({
+            errorMessage: "password와 confirmPassword가 일치하지 않습니다.",
+        });
+        return;
+    }
+    res.json({});
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
