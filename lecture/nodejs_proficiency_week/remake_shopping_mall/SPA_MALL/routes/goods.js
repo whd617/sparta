cb180986f2c423b7d0cd31788f67f2133eaabf11
Jonghyeon Choi <whd617@naver.com>
2023-11-13 00:05:58 +0900

    routes goods schekmas cart revise

--- lecture/nodejs_proficiency_week/remake_shopping_mall/SPA_MALL/routes/goods.js
+++ lecture/nodejs_proficiency_week/remake_shopping_mall/SPA_MALL/routes/goods.js
@@ -2,112 +2,133 @@ const express = require("express");
 const router = express.Router();
 
 const goods = [
-  {
-    goodsId: 4,
-    name: "상품 4",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-    category: "drink",
-    price: 0.1,
-  },
-  {
-    goodsId: 3,
-    name: "상품 3",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-    category: "drink",
-    price: 2.2,
-  },
-  {
-    goodsId: 2,
-    name: "상품 2",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-    category: "drink",
-    price: 0.11,
-  },
-  {
-    goodsId: 1,
-    name: "상품 1",
-    thumbnailUrl:
-      "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-    category: "drink",
-    price: 6.2,
-  },
+    {
+        goodsId: 4,
+        name: "상품 4",
+        thumbnailUrl: "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
+        category: "drink",
+        price: 0.1,
+    },
+    {
+        goodsId: 3,
+        name: "상품 3",
+        thumbnailUrl: "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
+        category: "drink",
+        price: 2.2,
+    },
+    {
+        goodsId: 2,
+        name: "상품 2",
+        thumbnailUrl: "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
+        category: "drink",
+        price: 0.11,
+    },
+    {
+        goodsId: 1,
+        name: "상품 1",
+        thumbnailUrl: "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
+        category: "drink",
+        price: 6.2,
+    },
 ];
 
 //상품 목록 조회 API
-router.get("/goods", (req, res) => {
-  res.json({ goods: goods });
+router.get("/goods", async (req, res) => {
+    const { category } = req.query;
+
+    const goods = await Goods.find(category ? { category } : {})
+        .sort("-date")
+        .exec();
+
+    const results = goods.map((item) => {
+        return {
+            goodsId: item.goodsId,
+            name: item.name,
+            price: item.price,
+            thumbnailUrl: item.thumbnailUrl,
+            category: item.category,
+        };
+    });
+
+    res.json({ goods: results });
 });
 
 //상품 상세 조회 API
-router.get("/goods/:goodsId", (req, res) => {
-  const { goodsId } = req.params;
-  const [detail] = goods.filter((goods) => goods.goodsId === Number(goodsId));
-  res.json({ detail });
+router.get("/goods/:goodsId", async (req, res) => {
+    const { goodsId } = req.params;
+
+    const goods = await Goods.findOne({ goodsId: goodsId }).exec();
+
+    if (!goods) return res.status(404).json({});
+
+    const result = {
+        goodsId: goods.goodsId,
+        name: goods.name,
+        price: goods.price,
+        thumbnailUrl: goods.thumbnailUrl,
+        category: goods.category,
+    };
+
+    res.json({ goods: result });
 });
 
 const Cart = require("../schemas/cart.js");
 
 router.post("/goods/:goodsId/cart", async (req, res) => {
-  const { goodsId } = req.params;
-  const { quantity } = req.body;
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
 
-  const existsCarts = await Cart.find({ goodsId });
-  if (existsCarts.length) {
-    return res.status(400).json({
-      success: false,
-      errorMessage: "이미 장바구니에 해당하는 상품이 존재합니다.",
-    })
-  }
+    const existsCarts = await Cart.find({ goodsId });
+    if (existsCarts.length) {
+        return res.status(400).json({
+            success: false,
+            errorMessage: "이미 장바구니에 해당하는 상품이 존재합니다.",
+        });
+    }
 
-  await Cart.create({ goodsId, quantity });
+    await Cart.create({ goodsId, quantity });
 
-  res.json({ result: "success" });
-})
+    res.json({ result: "success" });
+});
 
 router.put("/goods/:goodsId/cart", async (req, res) => {
-  const { goodsId } = req.params;
-  const { quantity } = req.body;
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
 
-  const existsCarts = await Cart.find({ goodsId });
-  if (existsCarts.length) {
-    await Cart.updateOne(
-      { goodsId: goodsId },
-      { $set: { quantity: quantity } }
-    )
-  }
-  res.status(200).json({ success: true });
-})
+    const existsCarts = await Cart.find({ goodsId });
+    if (existsCarts.length) {
+        await Cart.updateOne({ goodsId: goodsId }, { $set: { quantity: quantity } });
+    }
+    res.status(200).json({ success: true });
+});
 
 router.delete("/goods/:goodsId/cart", async (req, res) => {
-  const { goodsId } = req.params;
+    const { goodsId } = req.params;
 
-  const existsCarts = await Cart.find({ goodsId });
-  if (existsCarts.length) {
-    await Cart.deleteOne({ goodsId });
-  }
+    const existsCarts = await Cart.find({ goodsId });
+    if (existsCarts.length) {
+        await Cart.deleteOne({ goodsId });
+    }
 
-  res.json({ result: "success" });
-})
+    res.json({ result: "success" });
+});
 
 const Goods = require("../schemas/goods.js");
 router.post("/goods", async (req, res) => {
-  const { goodsId, name, thumbnailUrl, category, price } = req.body;
+    const { goodsId, name, thumbnailUrl, category, price } = req.body;
 
-  const goods = await Goods.find({ goodsId });
+    const goods = await Goods.find({ goodsId });
 
-  if (goods.length) {
-    return res.status(400).json({
-      success: false,
-      errorMessage: "이미 존재하는 GoodsId입니다."
-    });
-  }
+    if (goods.length) {
+        return res.status(400).json({
+            success: false,
+            errorMessage: "이미 존재하는 GoodsId입니다.",
+        });
+    }
 
-  const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
+    const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
 
-  res.json({ goods: createdGoods });
-})
+    res.json({ goods: createdGoods });
+});
 
 module.exports = router;
--- lecture/nodejs_proficiency_week/remake_shopping_mall/SPA_MALL/schemas/cart.js
+++ lecture/nodejs_proficiency_week/remake_shopping_mall/SPA_MALL/schemas/cart.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-  goodsId: {
-    type: Number,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  }
+    userId: {
+        type: String,
+        required: true,
+    },
+    goodsId: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
 });
 
 module.exports = mongoose.model("Cart", cartSchema);
